2fca73de0490a96495f3cab3dd91d7dae91a9e89
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-04-06 10:50:08 -0700

    feat: create a list of positive words to use

--- sentiment_classifier.py
+++ sentiment_classifier.py
@@ -1,5 +1,13 @@
 punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
 
+# list of positive words to use
+positive_words = []
+with open("positive_words.txt") as pos_f:
+    for lin in pos_f:
+        if lin[0] != ';' and lin[0] != '\n':
+            positive_words.append(lin.strip())
+            
+
 def strip_punctuation(s):
     '''
     (string) -> string
